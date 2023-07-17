208252a80d1fe733d9fab735d38ab29b495e5080
JoseFilipeVieiraFurieri <jf.furieri@ymail.com>
2023-07-17 03:31:30 -0300

    feat: checkpoint 3 adapters

--- chatblog/chat/views.py
+++ chatblog/chat/views.py
@@ -1,8 +1,31 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from chatterbot import ChatBot
+from django.contrib.auth import authenticate, login
+
+
+ChatBot('chatblog', read_only=False, 
+        storage_adapter="chatterbot.storage.SQLStorageAdapter",
+        logic_adapters=[
+
+])
+
+
 def index(request):
     return render(request, "chat/index.html")
 
+def getResponse(request):
+    UserMessage = request.GET.get('userMessage')
+    return HttpResponse(UserMessage)
+
+def authenticate(request):
+    username = request.POST["username"]
+    password = request.POST["password"]
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        login(request, user)
+    else:
+        return HttpResponse('invalid Login')
 
 
