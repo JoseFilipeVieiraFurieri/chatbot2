3ca3744fa08091261d59be343e11eba71b089de5
JoseFilipeVieiraFurieri <jf.furieri@ymail.com>
2023-07-17 02:26:40 -0300

    feat:project check 2

--- chatblog/chat/views.py
+++ chatblog/chat/views.py
@@ -2,5 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('this is my url')
+    return render(request, "chat/index.html")
+
+
 
