80a0ddb350e49aec32bbef698c9c9e5fae12e1ce
JoseFilipeVieiraFurieri <jf.furieri@ymail.com>
2023-07-17 22:31:09 -0300

    feat:checkpoint point 3 ta respondendo

--- chatblog/chat/views.py
+++ chatblog/chat/views.py
@@ -1,31 +1,45 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-from chatterbot import ChatBot
 from django.contrib.auth import authenticate, login
 
 
-ChatBot('chatblog', read_only=False, 
+from chatterbot import ChatBot
+from django.contrib.auth import authenticate, login
+from .chat_adapters import Login_User
+from chatterbot.conversation import Statement
+import logging
+
+
+bot = ChatBot('chatblog', read_only=False,
+              
+ 
         storage_adapter="chatterbot.storage.SQLStorageAdapter",
         logic_adapters=[
 
-])
 
+        ],
+        database_uri='sqlite:///database.sqlite3')
+
+print('Type something to begin...')
+
+# The following loop will execute each time the user enters input
+
+
+
+
+
+
+userAdapter = Login_User(bot)
+
+bot.logic_adapters.append(userAdapter)
+
+print(bot.logic_adapters)
 
 def index(request):
     return render(request, "chat/index.html")
 
+
 def getResponse(request):
-    UserMessage = request.GET.get('userMessage')
-    return HttpResponse(UserMessage)
-
-def authenticate(request):
-    username = request.POST["username"]
-    password = request.POST["password"]
-    user = authenticate(request, username=username, password=password)
-    if user is not None:
-        login(request, user)
-    else:
-        return HttpResponse('invalid Login')
-
-
+    userMessage = request.GET.get("userMessage")
+    ChatBot = str(bot.get_response(userMessage))
+    return HttpResponse(ChatBot)
