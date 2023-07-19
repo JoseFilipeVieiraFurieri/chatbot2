111f2555df0638ba727c216faccd672650d03434
JoseFilipeVieiraFurieri <jf.furieri@ymail.com>
2023-07-19 17:34:51 -0300

    feat:chatbot

--- chatblog/chat/views.py
+++ chatblog/chat/views.py
@@ -1,39 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.contrib.auth import authenticate, login
-
+from django.utils.html import format_html
 
 from chatterbot import ChatBot
-from django.contrib.auth import authenticate, login
-from .chat_adapters import Login_User
-from chatterbot.conversation import Statement
-import logging
-
+from chatterbot.trainers import ListTrainer, ChatterBotCorpusTrainer
 
 bot = ChatBot('chatblog', read_only=False,
               
  
         storage_adapter="chatterbot.storage.SQLStorageAdapter",
         logic_adapters=[
-
-
+          {
+          "import_path": "chatterbot.logic.BestMatch",
+          "default_response": "Sorry, i have no response to that answer",
+          "maximum_similarity_threshold": 0.95
+          }
         ],
         database_uri='sqlite:///database.sqlite3')
 
-print('Type something to begin...')
 
-# The following loop will execute each time the user enters input
-
-
+corpus = ChatterBotCorpusTrainer(bot)
+corpus.train('chatterbot.corpus.english')
 
 
-
-
-userAdapter = Login_User(bot)
-
-bot.logic_adapters.append(userAdapter)
-
-print(bot.logic_adapters)
 
 def index(request):
     return render(request, "chat/index.html")
